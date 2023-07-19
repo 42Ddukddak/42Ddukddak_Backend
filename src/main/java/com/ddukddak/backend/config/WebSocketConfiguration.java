a52cc30e7d40ad115d0bece9d3288edc6280dc98
saewoo1 <wooozooo1123@gmail.com>
2023-07-19 14:40:20 +0900

    [feat]: websock set

--- src/main/java/com/ddukddak/backend/config/WebSocketConfiguration.java
+++ src/main/java/com/ddukddak/backend/config/WebSocketConfiguration.java
@@ -1,11 +1,9 @@
 package com.ddukddak.backend.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-//@Configuration
 @Component
 public class WebSocketConfiguration {
 
