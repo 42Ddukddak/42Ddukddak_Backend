9fd8c6a8706a896b6ce69bfe07827e12ec2b8bac
saewoo1 <wooozooo1123@gmail.com>
2023-07-19 19:17:29 +0900

    [feat]: WebsocketConfig -> stompConfig

---
+++ src/main/java/com/ddukddak/backend/config/StompWebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.ddukddak.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+//@Component
+@Configuration
+@EnableWebSocket
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    /*
+    * /ws -> WebSock이 웹소켓 핸드셰이크 커넥션 생성 경로
+    * /test 경로로 시작하는 STOMP 메세지의 "destination" 헤더는 @Controller의 @MessageMapping 메세지로 라우팅.
+    * 내장된 메세지 브로커 -> 1.Client에게 Subscriptions, Broadcasting 기능 제공.
+    *                   2./topic, /queue로 시작하는 "destination" 헤더를 가진 메세지를 브로커로 라우팅한다.
+    * */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/test");
+        config.enableSimpleBroker("/topic", "/queue");
+    }
+
+
+}
